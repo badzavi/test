9524c8e893dcefb97d6961fd8113b5ba27307da4
unknown <@badzavi>
2023-02-12 19:51:07 +0600

    first

---
+++ test9/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('main_content')
+
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal">About us</h1>
+        <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with
+            this Bootstrap example. It’s built with default Bootstrap components and utilities with
+            little customization.</p>
+    </div>
+
+@endsection
